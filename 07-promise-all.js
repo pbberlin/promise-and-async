62e2f96b8727f866a8c9ecf653d9b36d5f77275b
Peter Buchmann <peter.buchmann@zew.de>
2022-01-13 21:59:53 +0100

    tiny

--- 07-promise-all.js
+++ 07-promise-all.js
@@ -3,14 +3,20 @@ import fetch from 'node-fetch';
 
 
 {
-    const p1 = Promise.resolve(3);
-    const p2 = 2;
-    const p3 = new Promise(  resv => setTimeout(resv, 100, 1)  );
+    const p1 = Promise.resolve(1);
+    const p2 = Promise.resolve().then( () => 2 );
+    const p3 = new Promise(  resv => setTimeout(resv, 100, 3)  );
 
 
     // return value order will be in order of the Promises passed - regardless of completion order
-    Promise.all(  [p1, p2, p3]  ).then(  vals => console.log(vals)  );
-    Promise.all(  [p3, p2, p1]  ).then(  vals => console.log(vals)  );
+    Promise.all(  [p3, p2, p1]  ).then(  vals => console.log("(1)", vals)  );
+
+    // actual resolution order
+    let ress = [];
+    [p3, p2, p1].forEach( p => {
+        p.then(  v => ress.push(v)  ).then( () => console.log("(2)", ress)  ); 
+    });
+
 }
 
 
